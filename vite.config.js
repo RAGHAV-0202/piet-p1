53d5429ccc72625c7805a2b0d648c84ee981dd17
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2025-03-08 20:21:32 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  esbuild: {
+    jsx: 'react',  // Telling esbuild to treat .js files as JSX with React
+  },
+})
