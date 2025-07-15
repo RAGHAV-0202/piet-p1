2d3195bf23fcb425cd65ef4cd4c47c789a822446
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2025-07-16 01:17:52 +0530

    Update baseurl.js

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,5 +1,5 @@
 // const baseUrl = "http://localhost:4000/"
-const baseUrl = "https://piet-p1-production.up.railway.app/"
+const baseUrl = "https://piet-p1-backend-production.up.railway.app/"
 
 
 export default baseUrl
