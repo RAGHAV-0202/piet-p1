d7b65bcf59c4c1f9db12ac90f544bf1464e2510b
Raghav Kapoor <101061750+RAGHAV-0202@users.noreply.github.com>
2026-02-19 10:17:42 +0530

    Update baseurl.js

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,6 +1,6 @@
 // const baseUrl = "http://localhost:4000/"
 // const baseUrl = "https://piet-p1-backend-production.up.railway.app/"
-const baseUrl = "https://piet-p1-fbd329b2a1c0.herokuapp.com"
+const baseUrl = "https://piet-p1-fbd329b2a1c0.herokuapp.com/"
 
 
 export default baseUrl
